715c1b5b48d8fdfb459cd8c6436d0b430b6fa39e
MikaJanBales <114147741+MikaJanBales@users.noreply.github.com>
2023-03-02 22:41:37 +0300

    add postgres and docker

--- online_school_foxford/online_school_foxford/settings.py
+++ online_school_foxford/online_school_foxford/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'online_school_foxford.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('POSTGRES_DB', 'foxford'),
+        'USER': os.getenv('POSTGRES_USER', 'foxford'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'foxford'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
